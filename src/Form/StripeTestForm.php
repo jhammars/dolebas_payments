deaa6313fce7e94dff58bf966404bcc5c47c3b87
Chintan Vyas <chintan.vyas89@gmail.com>
2017-05-26 14:22:20 +0530

    Adding stripe test form

---
+++ src/Form/StripeTestForm.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Drupal\dolebas_payments\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class StripeTestForm.
+ *
+ * @package Drupal\dolebas_payments\Form
+ */
+class StripeTestForm extends FormBase {
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'stripe_test_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['currency'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Currency'),
+      '#maxlength' => 64,
+      '#size' => 64,
+    ];
+    $form['amount'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Amount'),
+      '#description' => $this->t('Amount'),
+    ];
+    $form['fee'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Fee'),
+      '#description' => $this->t('Fee'),
+    ];
+    $form['fee_email'] = [
+      '#type' => 'email',
+      '#title' => $this->t('Fee Email'),
+      '#description' => $this->t('Fee Email'),
+    ];
+    $form['receiver_email'] = [
+      '#type' => 'email',
+      '#title' => $this->t('Receiver Email'),
+      '#description' => $this->t('Receiver Email'),
+    ];
+    $form['sender_email'] = [
+      '#type' => 'email',
+      '#title' => $this->t('Sender Email'),
+      '#description' => $this->t('Sender Email'),
+    ];
+    $form['processor'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Processor'),
+      '#description' => $this->t('Processor'),
+      '#maxlength' => 64,
+      '#size' => 64,
+    ];
+    $form['payment_id'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Payment Id'),
+      '#description' => $this->t('Payment Id'),
+      '#maxlength' => 64,
+      '#size' => 64,
+    ];
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Display result.
+    foreach ($form_state->getValues() as $key => $value) {
+      drupal_set_message($key . ': ' . $value);
+    }
+
+  }
+
+}
